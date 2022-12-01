ffae813b76b45fc8682fa7c524ac4cd7666d889c
Kitty Baugh <kexinkittygu@gmail.com>
2022-12-01 13:16:57 +0000

    Day 1 lazy ass python code

---
+++ 2022/day01.py
@@ -0,0 +1,11 @@
+with open('input/day01', 'r') as f:
+    s = f.read()
+
+groups = s.split('\n\n')
+e_sum = [sum([int(i) for i in e.split()]) for e in groups]
+
+# Task 1
+print(max(e_sum))
+
+# Task 2
+print(sum(sorted(e_sum, reverse=True)[:3]))
